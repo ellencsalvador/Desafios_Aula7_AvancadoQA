8bf1507fd6db5694d9df330dbdbcdf3fe5e8ad1c
xxDerpyXD <lapsdon54@gmail.com>
2025-10-21 20:36:50 -0300

    Adiciona Aula3: Desafios (tupla, collections, if)

---
+++ Aula3/Desafio_if.py
@@ -0,0 +1,20 @@
+idade = 25
+if idade > 18:
+    print("Indivíduo possui idade mínima para dirigir")
+
+
+idade = 17
+if idade > 18:
+    print("Indivíduo possui idade mínima para dirigir")
+else:
+    print("Indivíduo NÃO possui idade mínima para dirigir")
+
+
+idade = 18
+if idade > 18:
+  print("Indivíduo possui idade mínima para dirigir")
+elif idade == 17 or idade == 18:
+  print("Indivíduo tem entre 17 e 18 anos e ainda NÃO está apto para dirigir")
+else:
+  print("Indivíduo NÃO possui idade mínima para dirigir")
+
